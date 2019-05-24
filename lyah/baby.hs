3455d0ef151ebf076c4dcca377ecaf8629bb411e
Nathan Givens <nate.givens03@gmail.com>
2019-05-24 08:42:22 -0400

    starting lists

--- lyah/baby.hs
+++ lyah/baby.hs
@@ -5,3 +5,11 @@ doubleUs x y = (doubleMe x) + (doubleMe y)
 doubleSmallNumber x = if x > 100
                          then x
                          else x*2
+
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+
+-- function names cannot begin with an upper case letter
+-- They can include "'" though
+conanO'Brien = "Its a-me, Conan O'Brien!"
+
+concatList = [1, 2] ++ [3, 4]
