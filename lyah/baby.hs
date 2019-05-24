7b3e1007b861db78b7ee13df5a2b6a6155cfd287
Nathan Givens <nate.givens03@gmail.com>
2019-05-24 17:24:05 -0400

    Working through higher order functions

--- lyah/baby.hs
+++ lyah/baby.hs
@@ -13,3 +13,176 @@ doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
 conanO'Brien = "Its a-me, Conan O'Brien!"
 
 concatList = [1, 2] ++ [3, 4]
+prependList = 1:[2, 3, 4, 5]
+retreiveLetter = "Steve Buscemi" !! 6
+retreiveNumber = [1, 2, 3, 4] !! 2
+getHead = head [1, 2, 3]
+restList = tail [1, 2, 3]
+getLast = last [1, 2, 3]
+getInit = init [1, 2, 3]
+getLength = length [1, 2, 3]
+isNull = null [1, 2, 3]
+reverseReverse = reverse [1, 2, 3]
+rightTriangles =
+  [ (a,b,c) | c <- [1..10], b <- [1..10], a <- [1..10], a^2 + b^2 == c^2 ]
+
+rightTriangles' =
+  [ (a,b,c) | c <- [1..10], b <- [1..10], a <- [1..10], a^2 + b^2 == c^2, a+b+c == 24 ]
+
+lucky :: (Integral a) => a -> String
+lucky 7 = "LUCKY NUMBER SEVEN!"
+lucky x = "Sorry, you're out of luck, pal."
+
+sayMe :: (Integral a) => a -> String
+sayMe 1 = "one"
+sayMe 2 = "two"
+sayMe 3 = "three"
+sayMe x = "nope"
+
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+head' :: [a] -> a
+head' [] = error "can't call head on an empty list"
+head' (x:_) = x
+
+tell :: (Show a) => [a] -> String
+tell [] = "This is an empty list"
+tell (x:[]) = "This list has one element: " ++ show x
+tell (x:y:[]) = "This list has two elements: " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "This is a long list. The first two elements are: " ++ show x ++ " and " ++ show y
+
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (_:xs) = 1 + length' xs
+
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
+
+firstLetter :: String -> String
+firstLetter "" = "Whoops, empty string"
+firstLetter all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
+
+bmiTell :: (RealFloat a) => a -> a -> String
+bmiTell weight height
+  | bmi <= 18.5 = "You're underweight"
+  | bmi <= 25.0 = "You're supposedly normal"
+  | bmi <= 30.0 = "You're fat"
+  | otherwise = "congrats, you're a whale"
+  where bmi = weight / height ^ 2
+
+max' :: (Ord a) => a -> a -> a
+max' a b
+  | a > b = a
+  | otherwise = b
+
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+  | a > b = GT
+  | a == b = EQ
+  | otherwise = LT
+
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis xs = [bmi w h | (w, h) <- xs]
+    where bmi weight height = weight / height ^ 2
+
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h =
+  let sideArea = 2 * pi * r * h
+      topArea = pi * r ^ 2
+   in sideArea + 2 * topArea
+
+calcBmis' :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis' xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]
+
+head2' :: [a] -> a
+head2' xs = case xs of [] -> error "No head for empty lists"
+                       (x:_) -> x
+
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs)
+  | x > maxTail = x
+  | otherwise = maxTail
+  where maxTail = maximum' xs
+
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+  | n <= 0 = []
+  | otherwise = x:replicate' (n - 1) x
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+  | n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x : take' (n - 1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+zip' :: [a] -> [b] -> [(a, b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x,y):zip' xs ys
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' a [] = False
+elem' a (x:xs)
+  | a == x = True
+  | otherwise = elem' a xs
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) = 
+  let smallerSorted = quicksort [a | a <- xs, a <= x]
+      biggerSorted = quicksort [a | a <- xs, a > x]
+   in smallerSorted ++ [x] ++ biggerSorted
+
+
+-- Higher order functions
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f y x= f x y
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
+  | p x = x : filter' p xs
+  | otherwise = filter' p xs
+
+quicksort' :: (Ord a) => [a] -> [a]
+quicksort' [] = []
+quicksort' (x:xs) =
+  let smallerSorted = quicksort' (filter (<= x) xs)
+      biggerSorted = quicksort' (filter (> x) xs)
+   in smallerSorted ++ [x] ++ biggerSorted
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+  | even n = n:chain (div n 2)
+  | odd n = n:chain(n*3 + 1)
+
+
